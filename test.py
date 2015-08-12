b572a6dc6b906742e5d0251c90484e16f077d565
Shivam <shivammutreja25@gmail.com>
2015-08-12 17:31:26 +0530

    scraped tennis scores

--- test.py
+++ test.py
@@ -1,18 +1,63 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-from textteaser import TextTeaser
+from nltk.tokenize import sent_tokenize,word_tokenize
+from nltk.corpus import stopwords
+from collections import defaultdict
+from string import punctuation
+from heapq import nlargest
 
-# article source: https://blogs.dropbox.com/developers/2015/03/limitations-of-the-get-method-in-http/
-title = "Limitations of the GET method in HTTP"
-text = "We spend a lot of time thinking about web API design, and we learn a lot from other APIs and discussion with their authors. In the hopes that it helps others, we want to share some thoughts of our own. In this post, we’ll discuss the limitations of the HTTP GET method and what we decided to do about it in our own API.  As a rule, HTTP GET requests should not modify server state. This rule is useful because it lets intermediaries infer something about the request just by looking at the HTTP method.  For example, a browser doesn’t know exactly what a particular HTML form does, but if the form is submitted via HTTP GET, the browser knows it’s safe to automatically retry the submission if there’s a network error. For forms that use HTTP POST, it may not be safe to retry so the browser asks the user for confirmation first.  HTTP-based APIs take advantage of this by using GET for API calls that don’t modify server state. So if an app makes an API call using GET and the network request fails, the app’s HTTP client library might decide to retry the request. The library doesn’t need to understand the specifics of the API call.  The Dropbox API tries to use GET for calls that don’t modify server state, but unfortunately this isn’t always possible. GET requests don’t have a request body, so all parameters must appear in the URL or in a header. While the HTTP standard doesn’t define a limit for how long URLs or headers can be, most HTTP clients and servers have a practical limit somewhere between 2 kB and 8 kB.  This is rarely a problem, but we ran up against this constraint when creating the /delta API call. Though it doesn’t modify server state, its parameters are sometimes too long to fit in the URL or an HTTP header. The problem is that, in HTTP, the property of modifying server state is coupled with the property of having a request body.  We could have somehow contorted /delta to mesh better with the HTTP worldview, but there are other things to consider when designing an API, like performance, simplicity, and developer ergonomics. In the end, we decided the benefits of making /delta more HTTP-like weren’t worth the costs and just switched it to HTTP POST.  HTTP was developed for a specific hierarchical document storage and retrieval use case, so it’s no surprise that it doesn’t fit every API perfectly. Maybe we shouldn’t let HTTP’s restrictions influence our API design too much.  For example, independent of HTTP, we can have each API function define whether it modifies server state. Then, our server can accept GET requests for API functions that don’t modify server state and don’t have large parameters, but still accept POST requests to handle the general case. This way, we’re opportunistically taking advantage of HTTP without tying ourselves to it."
+class FrequencySummarizer:
+  def __init__(self, min_cut=0.1, max_cut=0.9):
+    """
+     Initilize the text summarizer.
+     Words that have a frequency term lower than min_cut 
+     or higer than max_cut will be ignored.
+    """
+    self._min_cut = min_cut
+    self._max_cut = max_cut 
+    self._stopwords = set(stopwords.words('english') + list(punctuation))
 
-tt = TextTeaser()
+  def _compute_frequencies(self, word_sent):
+    """ 
+      Compute the frequency of each of word.
+      Input: 
+       word_sent, a list of sentences already tokenized.
+      Output: 
+       freq, a dictionary where freq[w] is the frequency of w.
+    """
+    freq = defaultdict(int)
+    for s in word_sent:
+      for word in s:
+        if word not in self._stopwords:
+          freq[word] += 1
+    # frequencies normalization and fitering
+    m = float(max(freq.values()))
+    for w in freq.keys():
+      freq[w] = freq[w]/m
+      if freq[w] >= self._max_cut or freq[w] <= self._min_cut:
+        del freq[w]
+    return freq
 
-sentences = tt.summarize(title, text)
+  def summarize(self, text, n):
+    """
+      Return a list of n sentences 
+      which represent the summary of text.
+    """
+    sents = sent_tokenize(text)
+    assert n <= len(sents)
+    word_sent = [word_tokenize(s.lower()) for s in sents]
+    self._freq = self._compute_frequencies(word_sent)
+    ranking = defaultdict(int)
+    for i,sent in enumerate(word_sent):
+      for w in sent:
+        if w in self._freq:
+          ranking[i] += self._freq[w]
+    sents_idx = self._rank(ranking, n)    
+    return [sents[j] for j in sents_idx]
 
-for sentence in sentences:
-      print sentence
-
+  def _rank(self, ranking, n):
+    """ return the first n sentences with highest ranking """
+    return nlargest(n, ranking, key=ranking.get)
 
 
