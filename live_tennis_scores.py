958808f466ad3a9bbf27379d6b895eaa7bf769c2
Shivam <shivammutreja25@gmail.com>
2015-08-13 18:02:12 +0530

    updated tournaments in tennis

--- live_tennis_scores.py
+++ live_tennis_scores.py
@@ -11,17 +11,18 @@ class TennisScores:
 		self.rss = feedparser.parse(url)
 
 	def get_scores(self):
-		
+		new_tournaments = []
 		for feeds in self.rss.entries:
 			new_scores = []
 			new_sets = []
+                        tournaments = []
 			players = feeds['title'][39:].replace('#',"")
 			content = urlopen(feeds['id']).read()
 			soup = BeautifulSoup(content,'lxml')
 			for x in soup.find_all('table',{'class':'goalpop_rd'}):
 				scores = x.find_all('td',{'class':'bcen'})
 				sets = x.find_all('td',{'class':'cen'})
-				tournaments = x.find_all('td',{'class':'league'})
+				tournaments.append(x.find_all('td',{'class':'league'}))
 			for score in scores:
 				try:
 					new_scores.append(score.contents[0].string.strip())
@@ -33,12 +34,12 @@ class TennisScores:
 			for _set in sets:
 				new_sets.append(_set.string.strip())
 
-			for val in tournaments:
-				new_tournaments.append(val.contents[1].string.strip())			
+			for val in tournaments[0]:
+				new_tournaments.append(val.contents[1].string.strip())
 
 			print players
-			for _set,score1,score2 in itertools.izip(new_sets,new_scores[:5],new_scores[5:]):
-				print _set+"--> "+score1+' - '+score2
+			for tournament,_set,score1,score2 in itertools.izip(new_tournaments,new_sets,new_scores[:5],new_scores[5:]):
+				print tournament+' '+_set+"--> "+score1+' - '+score2
 			
 			 
 		
