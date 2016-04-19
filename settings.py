ef8f3389ccc32d9e6b1a0f6cf2055cf6d1676b13
aakarshi92 <aakarshi24@gmail.com>
2016-04-19 17:07:59 +0530

    pypy changes

--- settings.py
+++ settings.py
@@ -8,24 +8,14 @@ ERROR_404 = 404
 
 ERROR_LIST = [ERROR_500, ERROR_400, ERROR_402, ERROR_404]
 
-LOCAL_ELASTIC_SERVER = 'localhost'
+ELASTIC_SERVER = 'http://10.0.1.253'
 ELASTIC_PORT = 9200
-
-LOCAL_MONGO_HOST = 'localhost'
-
-# old mongo servers
-# MONGO_SERVERIP = 'mongodb://10.0.4.225:27017,10.0.2.249:27018, 10.0.2.117:27019/?replicaSet=fetchfeedReplicaSet'
 
 # new mongo servers
 MONGO_SERVERIP = 'mongodb://10.0.1.205:27017, 10.0.1.139:27017, 10.0.1.151:27017/?replicaSet=SportsNewsReplicaSet'
 MONGO_PORT = 27017
 
-# MONGO_SERVERIP = 'mongodb://localhost:27017'
-
 #SCORES server config
-SCORES_SERVERIP = '52.74.75.79'
-SCORES_SERVER_PORT = 8000
-
 SCORES_SERVERIP = 'http://ScoresLB-822670678.ap-northeast-2.elb.amazonaws.com'
 SCORES_SERVER_PORT = 80
 
