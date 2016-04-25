7a7994f9dd12eeb3dc2c057f36ca6a38ff5ab7f4
aakarshi92 <aakarshi24@gmail.com>
2016-04-25 18:20:11 +0530

    check 2

--- connection.py
+++ connection.py
@@ -14,7 +14,7 @@ def get_elastic_search_connection():
     global elastic_search_conn
     if not elastic_search_conn:
         elastic_search_conn = Elasticsearch(settings.ELASTIC_SERVER, timeout=30, maxsize=50) \
-            if pwd.getpwuid(os.getuid()) == 'ubuntu' else Elasticsearch()
+            if pwd.getpwuid(os.getuid()) == 'root' else Elasticsearch()
     return elastic_search_conn
 
 
@@ -26,7 +26,6 @@ def get_mongo_connection():
     monkey.patch_all()
     print 'getuser::::', getpass.getuser()
     print 'pwd user::', pwd.getpwuid(os.getuid())
-    print 'getlogin::::', os.getlogin()
     connection = pymongo.MongoClient(settings.MONGO_SERVERIP, settings.MONGO_PORT) \
-        if pwd.getpwuid(os.getuid()) == 'ubuntu' else pymongo.MongoClient()
+        if pwd.getpwuid(os.getuid()) == 'root' else pymongo.MongoClient()
     return connection
